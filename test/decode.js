4409f310ccba07fbeebba8f279f9a235092d59e1
Johnny Fee <djohnnyfee@gmail.com>
2016-11-20 22:22:58 +0800

    modify encode and decode, and readme

--- test/decode.js
+++ test/decode.js
@@ -6,8 +6,18 @@ var assert = require('assert');
 
 describe('Decode', function () {
 
-    it('decode', function () {
-        program.parse(['node', 'xdata-cli', 'decode', 'e4 b8 ad e5 9b bd', '-c', 'gbk']);
+    it('decode pretty hex', function () {
+        program.parse(['node', 'xdata-cli', 'decode', 'd6 d0 b9 fa', '--encoding', 'gbk']);
+        // 中国
+    });
+
+    it('decode compressed hex', function () {
+        program.parse(['node', 'xdata-cli', 'decode', 'd6d0b9fa', '-c', 'gbk']);
+        // 中国
+    });
+
+    it('decode array hex', function () {
+        program.parse(['node', 'xdata-cli', 'decode', '[0xd6, 0xd0, 0xb9, 0xfa]', '-c', 'gbk']);
         // 中国
     });
 });
--- test/encode.js
+++ test/encode.js
@@ -5,18 +5,18 @@ chai.should();
 describe('Encoding', function () {
 
     it('as default style', function () {
-        program.parse(['node', 'xdata-cli1', 'encode', '中国', '--encoding', 'gbk']);
+        program.parse(['node', 'xdata', 'encode', '中国', '--encoding', 'gbk']);
     });
 
     it('as array style', function () {
-        program.parse(['node', 'xdata-cli1', 'encode', '中国', '--encoding', 'gbk', '--format', 'array']);
+        program.parse(['node', 'xdata', 'encode', '中国', '--encoding', 'gbk', '--format', 'array']);
     });
 
     it('as hex map style', function () {
-        program.parse(['node', 'xdata-cli1', 'encode', '中国', '--encoding', 'gbk', '--format', 'map']);
+        program.parse(['node', 'xdata', 'encode', '中国', '--encoding', 'gbk', '--format', 'map']);
     });
 
     it('as pretty style', function () {
-        program.parse(['node', 'xdata-cli1', 'encode', '中国', '--encoding', 'gbk', '--format', 'pretty']);
+        program.parse(['node', 'xdata', 'encode', '中国', '--encoding', 'gbk', '--format', 'pretty']);
     });
 });
