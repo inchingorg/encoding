789540c42196aa5c6b898c022e5c07bed982bd73
Johnny Fee <djohnnyfee@gmail.com>
2016-11-20 22:49:08 +0800

    modify encode

--- test/decode.js
+++ test/decode.js
@@ -12,12 +12,12 @@ describe('Decode', function () {
     });
 
     it('decode compressed hex', function () {
-        program.parse(['node', 'xdata-cli', 'decode', 'd6d0b9fa', '-c', 'gbk']);
+        program.parse(['node', 'xdata-cli', 'decode', 'd6d0b9fa', '--encoding', 'gbk']);
         // 中国
     });
 
     it('decode array hex', function () {
-        program.parse(['node', 'xdata-cli', 'decode', '[0xd6, 0xd0, 0xb9, 0xfa]', '-c', 'gbk']);
+        program.parse(['node', 'xdata-cli', 'decode', '[0xd6, 0xd0, 0xb9, 0xfa]', '--encoding', 'gbk']);
         // 中国
     });
 });
